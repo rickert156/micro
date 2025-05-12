72f64c7372400441740393ecf37b00b0d4def9fe
rickert156 <rickert156@rambler.ru>
2025-05-12 10:58:20 +0400

    update boot to esp32

---
+++ esp32/README.md
@@ -0,0 +1,2 @@
+# Плата ESP32-WROOM 32
+![ESP32-C3 Supermini](https://github.com/rickert156/micro/blob/main/img/supermini-esp32-c3-pinout.jpg)
--- esp32/boot.py
+++ esp32/boot.py
@@ -1,60 +1,38 @@
-from machine import Pin
 from config import name_network, pass_network
-from lib.current_time import current_time
-import time, network
-
-def report(pause:int, ping:int):
-    pin = Pin(2, Pin.OUT)
-    count = 0
-    while count < ping:
-        count+=1
-        pin.value(1)
-        time.sleep(pause)
-        pin.value(0)
-        time.sleep(pause)
+import network, machine
+import time
 
 def log(update:str):
     with open('log.txt', 'a+') as file:
-        file.write(f'[{current_time()}] {update}\n')
+        file.write(f'{update}\n')
 
 def connect_wifi():
-    wifi = network.WLAN(network.STA_IF)
-    wifi.active(True)
-    
-    networks = wifi.scan()
-    status_search = False
-    update_name = ""
-    for net in networks:
-        if name_network in net[0].decode():
-            update_name = net[0].decode()
-            status_search = True
-    
-    counter_bad_connect = 0
-    status_connect = False
-    if status_search == True:
-        log(update="Сеть найдена!")
-        while status_connect == False:
-            wifi.connect(update_name, pass_network)
-            time.sleep(2)
-            if wifi.isconnected():
-                log(update=f"Подключился к Wi-Fi: {update_name}")
-                report(pause=0.1, ping=10)
-                status_connect = True
+    try:
+        wifi = network.WLAN(network.STA_IF)
+        wifi.active(True)
+        time.sleep(2)
+        networks = wifi.scan()
+
+        counter_detected = 0
+        
+        for net in networks:
+            if name_network == net[0].decode():
+                counter_detected+=1
+                if not wifi.isconnected():
+                    wifi.connect(name_network, pass_network)
+                    time.sleep(10)
+            
+                    if wifi.isconnected():
+                        log(update=f"Connect to: {name_network}")
+                    else:
+                        log(update=f"Not Connected to: {name_network}")
+
                 break
-            else:
-                log(update="Сеть найдена, но соединение не установлено")
-                counter_bad_connect+=1
-                report(pause=1, ping=5)
-                if counter_bad_connect == 10:
-                    break
+        if counter_detected == 0:
+            log(update=f"{name_network} not found")
 
-    else:
-        log(update="Сеть не найдена!")
-        report(pause=1, ping=10)
 
-    
-try:
-    connect_wifi()
-except Exception as err:
-    log(update=err)
+    except Exception as err:
+        log(update=err)
 
+connect_wifi()
